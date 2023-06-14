dd1d73cc4399513303bb23a9d2aec31529ff6f58
Alexey-Nov <98alexey@mail.ru>
2023-06-14 12:25:50 +0300

    commit

--- checkmate/js/script.js
+++ checkmate/js/script.js
@@ -98,9 +98,119 @@ document.addEventListener('DOMContentLoaded', function () {
 				type: 'bullets',
 				clickable: true,
 			},
+			breakpoints: {
+				0: {
+					spaceBetween: 10,
+					grid: {
+						rows: 2,
+					},
+				},
+				768: {
+					spaceBetween: 30,
+					grid: {
+						rows: 3,
+					},
+				},
+			}
 		});
 	};
 	sliderPartners()
+
+	// slider video
+	let sliderVideo = () => {
+		let SwiperVideo = new Swiper('.video__slider_active', {
+			slidesPerView: 2,
+			spaceBetween: 30,
+			loop: true,
+			navigation: {
+				nextEl: '.video__next',
+				prevEl: '.video__prev',
+			},
+			pagination: {
+				el: ".video__pagination",
+				type: 'bullets',
+				clickable: true,
+			},
+			breakpoints: {
+				0: {
+					slidesPerView: 1,
+				},
+				768: {
+					slidesPerView: 2,
+				},
+			}
+		});
+	};
+	sliderVideo()
+
+	// slider gallery
+	let sliderGallery = () => {
+		let SwiperGallery = new Swiper('.gallery__slider', {
+			slidesPerView: 3,
+			spaceBetween: 30,
+			loop: true,
+			navigation: {
+				nextEl: '.gallery__next',
+				prevEl: '.gallery__prev',
+			},
+			pagination: {
+				el: ".gallery__pagination",
+				type: 'bullets',
+				clickable: true,
+			},
+			breakpoints: {
+				0: {
+					slidesPerView: 1,
+				},
+				768: {
+					slidesPerView: 2,
+				},
+				1024: {
+					slidesPerView: 3,
+				},
+			}
+		});
+	};
+	sliderGallery()
+
+	// anchor
+	document.querySelectorAll('.anchor').forEach((el => {
+		el.addEventListener('click', (e) => {
+			e.preventDefault()
+			let currentAnchor = e.target.href.split('#').slice(1).join('')
+			let blockPosition = document.getElementById(currentAnchor).offsetTop
+
+			window.scroll({
+				top: blockPosition,
+				left: 0,
+				behavior: 'smooth'
+			});
+		})
+	}))
+
+	// read more 
+	document.querySelectorAll('.read-more').forEach(el => {
+		let btn = el.querySelector('.read-more__btn')
+		let text = el.querySelector('.read-more__text')
+
+		let limitHeight = +text.style.maxHeight.split('').slice(0, -2).join('')
+		let fullHeight = text.scrollHeight
+
+		if (limitHeight >= fullHeight) {
+			btn.remove()
+			el.classList.toggle('read-more_open')
+		}
+
+		btn.addEventListener('click', (e) => {
+			e.target.closest('.read-more_open') ? text.style.maxHeight = limitHeight + 'px' : text.style.maxHeight = fullHeight + 'px'
+
+			let currentText = btn.innerHTML
+			let toggleText = btn.getAttribute('data-toggle-text')
+			btn.setAttribute('data-toggle-text', currentText)
+			btn.innerText = toggleText
+			el.classList.toggle('read-more_open')
+		})
+	});
 
 });
 
