80376626508cc75f2e911c2b7be03f4230c829c1
Алексей Новокшонов <98alexey@mail.ru>
2023-09-05 22:20:38 +0300

    Delete react-app directory

--- react-app/src/components/UI/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './button.css';
-
-
-export default function Button({ children, ...props }) {
-    return (
-        <button {...props} className='btn'>{children}</button>
-    )
-}
--- react-app/src/components/UI/Input/Input.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { useState } from 'react'
-import './input.css'
-
-export default function Input({ ...props }) {
-    return (
-        <input {...props} className='input' ></input>
-    )
-}
